3e4bde28b0687c3cd03d9688d74e54f4fffde5bb
Isaac Karikari Oppong <isaackarikarioppong@gmail.com>
2026-08-28 07:50:40 +0000

    Complete JavaScript Milestone 1

---
+++ JavaScript/Milestone-1/script.js
@@ -0,0 +1,21 @@
+const name = "Isaac";
+let age = 20;
+
+// Array of hobbies
+const hobbies = ["Reading", "Gaming", "Hiking"];
+
+// Object representing a person
+const person = {
+    name: name,
+    age: age,
+    learning: true
+};
+
+//console.log statements to display/print the values of the variables, arrays, and objects
+console.log(name);
+console.log(age);
+console.log(hobbies);
+console.log(person);
+
+// Comparison
+console.log(age === 21);
